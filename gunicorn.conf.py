a07549efe320f572f872270a81f9c99759f15fb5
Xinruo Sun <xiaoruoruo@gmail.com>
2025-11-29 12:27:41 -0800

    capture log

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,6 +6,7 @@ workers = os.environ.get("WORKERS", "2")
 bind = os.environ.get("BIND", "0.0.0.0:8000")
 accesslog = os.environ.get("ACCESS_LOG", "-")
 errorlog = os.environ.get("ERROR_LOG", "-")
+capture_output = True
 
 # Disable timeout for long running LLM calls.
 timeout = 0
